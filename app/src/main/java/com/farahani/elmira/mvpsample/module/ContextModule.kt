c069783cfbcc262d2aa2573e5408127acfedd9ad
Elmira <elmira_frhn@hotmail.com>
2018-10-11 20:30:32 +0330

    add di and network modules

---
+++ app/src/main/java/com/farahani/elmira/mvpsample/module/ContextModule.kt
@@ -0,0 +1,19 @@
+package com.farahani.elmira.mvpsample.module
+
+import android.app.Application
+import android.content.Context
+import com.farahani.elmira.mvpsample.base.BaseView
+import dagger.Module
+import dagger.Provides
+
+@Module
+object ContextModule {
+
+    @Provides
+    @JvmStatic
+    internal fun provideContext(baseView: BaseView): Context = baseView.getContext()
+
+    @Provides
+    @JvmStatic
+    internal fun provideApplication(context: Context): Application = context.applicationContext as Application
+}
---
+++ app/src/main/java/com/farahani/elmira/mvpsample/module/NetworkModule.kt
@@ -0,0 +1,33 @@
+package com.farahani.elmira.mvpsample.module
+
+import com.farahani.elmira.mvpsample.BuildConfig
+import com.farahani.elmira.mvpsample.network.AlbumApi
+import dagger.Module
+import dagger.Provides
+import dagger.Reusable
+import io.reactivex.schedulers.Schedulers
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+@Module
+object NetworkModule {
+
+    @Provides
+    @Reusable
+    @JvmStatic
+    internal fun provideAlbumApi(retrofit: Retrofit): AlbumApi = retrofit.create(AlbumApi::class.java)
+
+    @Provides
+    @Reusable
+    @JvmStatic
+    internal fun provideRetrofitInterface(): Retrofit {
+
+        return Retrofit.Builder()
+                .baseUrl("${BuildConfig.BASE_URL}")
+                .addConverterFactory(MoshiConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+                .build()
+    }
+
+}
